9b7aa57824d034101a89959f7cf04abfbe17216c
Gianmarco Segura <gseguracordova@gmail.com>
2024-03-22 19:52:40 +0100

    feat: navigation with next links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { ReactNode } from "react"
 
 interface RootLayoutProps {
@@ -5,10 +6,25 @@ interface RootLayoutProps {
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
+    console.log('RootLayout Log')
     return (
         <html lang="en">
             <body>
-            <header>[Header]</header>
+            <header>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link href="/reviews">Reviews</Link>
+                        </li>
+                        <li>
+                            <Link href="/about">About</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
                 { children }
             <footer>[Footer]</footer>
             </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 export default function HomePage() {
+    console.log('HomePage Log')
     return (
         <h1>My first Next.jss page</h1>
     )
